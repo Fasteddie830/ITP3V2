cd085062df33de7ee8139f51b4dec6d1009bff8c
KEVINURAMA <kurama200@caledonian.ac.uk>
2022-04-25 23:03:25 +0100

    chages

--- models/Events.js
+++ models/Events.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose')
 const Event = new mongoose.Schema({
     Title: {type: String, default:''},
     Description: {type: String, default:''},
+    day_day: {type: Number, default: 0},
+    day_month: {type: Number, default: 0},
+    day_year: {type: Number, default: 0},
     timestamp: {type: Date, default: Date.now},
     img:
     {
